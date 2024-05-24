42f4a770570a93bb14d35fb9fbaab0b041392e2d
a-hr <heerreroalvaro@gmail.com>
2024-05-24 10:17:05 +0200

    added example data

--- AS-VAST_tools/3_analysis.sh
+++ AS-VAST_tools/3_analysis.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash -l
 
+project_dir=/scratch/heral/Salmon-VAST_tools/AS-VAST_tools
+
 # ---- INPUTS ----
+cd $project_dir
 
-input_table_filt="/Users/varo/Library/Mobile Documents/com~apple~CloudDocs/Biodonostia/GB/AS_processing/data/coverage_variance_filtered_INCLUSION_LEVELS_CGGA.tab.gz"
-input_table_full="/Users/varo/Library/Mobile Documents/com~apple~CloudDocs/Biodonostia/GB/AS_processing/data/INCLUSION_LEVELS_FULL-hg38-121_CGGA.tab.gz"
-design_tab="/Users/varo/Library/Mobile Documents/com~apple~CloudDocs/Biodonostia/GB/AS_processing/designs/design_CGGA.tab"
+input_table_filt="$project_dir/data/coverage_filtered_INCLUSION_LEVELS_BIOD.tab.gz"
+input_table_full="$project_dir/data/INCLUSION_LEVELS_FULL-hg38-78_BIOD.tab.gz"
+design_tab="$project_dir/designs/design_BIOD.tab"
+group_A=Normal_brain
+group_B=GB_tumor
 
-group_A=Oligodendroglioma_G2
-group_B=Glioblastoma
-
-output_dir="/Users/varo/Library/Mobile Documents/com~apple~CloudDocs/Biodonostia/GB/AS_processing/perl_R_python"
+output_dir="$project_dir/perl_python"
 
 min_comparison_pct=50
 min_dpsi=10
@@ -20,7 +22,7 @@ min_dpsi=10
 # conda create -n AS-env
 # conda install -c conda-forge pandas numpy plotly python-kaleido matplotlib scipy
 
-conda activate AS-env
+# conda activate /scratch/$USER/envs/AS_env
 
 # ---- RUN ----
 python AS_analysis.py "${input_table_filt}" "${input_table_full}" "${design_tab}" "${group_A}" "${group_B}" "${output_dir}" "${min_comparison_pct}" "${min_dpsi}"
